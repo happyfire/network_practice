e868866108be5e8bf06ae0ef2a9bc3d5d9d3ba85
wuhao <hao.wu@dreamingame.net>
2017-06-14 17:26:42 +0800

    udp connect后获取外出接口

--- echo/udpcli02.c
+++ echo/udpcli02.c
@@ -26,8 +26,19 @@ void dg_cli(FILE *fp, int sockfd, const struct sockaddr* pservaddr, socklen_t se
 {
 	int n;
 	char sendline[MAXLINE], recvline[MAXLINE+1];
+	struct sockaddr_in cliaddr;
+	socklen_t len;
 
 	Connect(sockfd, (struct sockaddr*)pservaddr, servlen);
+
+	//调用connect后，可通过getsockname获得输出接口的ip地址和端口
+	len = sizeof(cliaddr);
+	getsockname(sockfd,(struct sockaddr*)&cliaddr, &len);
+	char localAddr[INET_ADDRSTRLEN];
+	Inet_ntop(AF_INET, &(cliaddr.sin_addr.s_addr), localAddr, INET_ADDRSTRLEN);
+
+	printf("local address %s:%d\n", localAddr,ntohs(cliaddr.sin_port));
+
 
 	while(Fgets(sendline, MAXLINE, fp)!=NULL){
 		Write(sockfd, sendline, strlen(sendline));
