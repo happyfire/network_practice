dd95377ed532c969e472d9a07530b4d81a2cfc9b
wuhao <hao.wu@dreamingame.net>
2017-06-14 16:44:17 +0800

    echo: udpcli02: using connect for UDP

---
+++ echo/udpcli02.c
@@ -0,0 +1,40 @@
+#include "netfoo.h"
+
+void dg_cli(FILE *fp, int sockfd, const struct sockaddr* pservaddr, socklen_t servlen);
+
+int main(int argc, char **argv)
+{
+	int sockfd;
+	struct sockaddr_in servaddr;
+
+	if(argc!=2)
+		err_quit("usage: udpcli <IPaddress>");
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(SERV_PORT);
+	Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+
+	sockfd = Socket(AF_INET, SOCK_DGRAM, 0);
+
+	dg_cli(stdin, sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
+}
+
+//相对于上一个版本 udpcli01.c，使用了udp connect
+
+void dg_cli(FILE *fp, int sockfd, const struct sockaddr* pservaddr, socklen_t servlen)
+{
+	int n;
+	char sendline[MAXLINE], recvline[MAXLINE+1];
+
+	Connect(sockfd, (struct sockaddr*)pservaddr, servlen);
+
+	while(Fgets(sendline, MAXLINE, fp)!=NULL){
+		Write(sockfd, sendline, strlen(sendline));
+
+		n = Read(sockfd, recvline, MAXLINE);
+
+		recvline[n] = 0; //null terminate
+		Fputs(recvline, stdout);
+	}
+}
